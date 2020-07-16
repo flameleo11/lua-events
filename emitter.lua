96330a814e0724cb750c33e6e336084525f7feb4
flameleo11 <flameleo11@foxmail.com>
2020-07-17 03:12:05 +0800

    enable fn , consider whether it useful

--- emitter.lua
+++ emitter.lua
@@ -3,69 +3,72 @@ local pfx_once = '_once_'
 local Emitter = {}
 
 setmetatable(Emitter, {
-	__call = function (_) 
-		return Emitter:new() 
-	end
+  __call = function (_) 
+    return Emitter:new() 
+  end
 })
 
 local meta_obj = {
-	__index = Emitter
+  __index = Emitter
 }
 
 function Emitter:new()
-	local obj = {}
-	setmetatable(obj, meta_obj)
-	obj:init()
-	return obj
+  local obj = {}
+  setmetatable(obj, meta_obj)
+  obj:init()
+  return obj
 end
 
 function Emitter:init()
-	self._event_fn = {}
+  self._event_fn = {}
 end
 
 -- there is no need to add same
 -- listener twice for same event
 function Emitter:addListener(event, fn)
-	self._event_fn[event] = self._event_fn[event] or {}
-	self._event_fn[event][fn] = true
+  self._event_fn[event] = self._event_fn[event] or {}
+  self._event_fn[event][fn] = true
 end
 
 function Emitter:enable(event, fn, enable)
-	self._event_fn[event][fn] = false
+  if (  self._event_fn[event] 
+    and self._event_fn[event][fn] ~= nil) then
+    self._event_fn[event][fn] = enable 
+  end
 end
 
 function Emitter:_once_name(event)
-	return pfx_once..event;
+  return pfx_once..event;
 end
 
 function Emitter:once(event, fn)
-	self:addListener(self:_once_name(event), fn)
+  self:addListener(self:_once_name(event), fn)
 end
 
 function Emitter:_dispatch(event, ...)
-	local fn_dict = self._event_fn[event];
-	if not (fn_dict) then
-		return 
-	end
+  local fn_dict = self._event_fn[event];
+  if not (fn_dict) then
+    return 
+  end
 
-	-- one of fns throw error 
-	-- do not stop other emit
-	local params = {...}
-	for fn, enable in pairs(fn_dict) do
-		if (enable) then
-			xpcall(function ()
-				fn(unpack(params))
-			end, print)
-		end
-	end
+  -- one of fns throw error 
+  -- do not stop other emit
+  local params = {...}
+  for fn, enable in pairs(fn_dict) do
+    if (enable) then
+      xpcall(function ()
+        fn(unpack(params))
+      end, print)
+    end
+  end
 end
 
 function Emitter:emit(event, ...)
-	self:_dispatch(event, ...)
+  self:_dispatch(event, ...)
 
   local once_event = self:_once_name(event)
   self:_dispatch(once_event, ...)
-	self._event_fn[once_event] = nil;
+  self._event_fn[once_event] = nil;
 end
 
 function Emitter:removeAllListeners(event)
@@ -74,14 +77,14 @@ function Emitter:removeAllListeners(event)
 end
 
 function Emitter:_clear(event, fn)
-	if (fn and self._event_fn[event]) then
-		self._event_fn[event][fn] = nil 
-	end
+  if (fn and self._event_fn[event]) then
+    self._event_fn[event][fn] = nil 
+  end
 end
 
 function Emitter:removeListener(event, fn)
-	self:_clear(event, fn)
-	self:_clear(self:_once_name(event), fn)
+  self:_clear(event, fn)
+  self:_clear(self:_once_name(event), fn)
 end
 
 Emitter.on = Emitter.addListener
